8c4c056853fb1b22382dfb39c45b5d7eab252f60
jouchuki <95777300+jouchuki@users.noreply.github.com>
2023-11-08 15:52:33 +0100

    Translation update:  - added file translate.py that contains a working pipeline for translation, though some further development is needed  - removed all unnecessary files and directories

--- arxiv_reco/translate.py
+++ arxiv_reco/translate.py
@@ -5,7 +5,7 @@ translator = ctranslate2.Translator("C:/Users/vsoko/Downloads/ct_model")
 sp_source = spm.SentencePieceProcessor("C:/Users/vsoko/Downloads/source.model")
 sp_target = spm.SentencePieceProcessor("C:/Users/vsoko/Downloads/target.model")
 
-input_text = "Hello world!"
+input_text = ""
 input_tokens = sp_source.encode(input_text, out_type=str)
 
 results = translator.translate_batch([input_tokens])
