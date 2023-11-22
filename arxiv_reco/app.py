af208e6844c2a4f4ed0c62e6d3d848075ef5fe43
jouchuki <95777300+jouchuki@users.noreply.github.com>
2023-11-22 11:36:58 +0100

    Front-End Using Streamlit patch 4

--- arxiv_reco/app.py
+++ arxiv_reco/app.py
@@ -6,32 +6,40 @@ import streamlit as st
 # streamlit run C:/Users/vsoko/PycharmProjects/SEProject/Software-Engineering-Project-Group-6/arxiv_reco/app.py
 
 
+import streamlit as st
+
 def main():
-    # Instantiate batch_size --- amount of articles shown in one batch
+    # Step 1: Initialize Session State Variables
     if 'batch_size' not in st.session_state:
         st.session_state['batch_size'] = 10
 
-    # Prompt for keywords
     if 'keywords' not in st.session_state:
+        st.session_state['keywords'] = None
+
+    if 'articles' not in st.session_state:
+        st.session_state['articles'] = None
+
+    if 'reco_list' not in st.session_state:
+        st.session_state['reco_list'] = None
+
+    # Step 2: Keyword Input
+    if not st.session_state['keywords']:
         st.session_state['keywords'] = st_prompt_for_keywords()
 
-        # Proceed only if keywords are provided
-        if st.session_state.keywords:
-            st.session_state['articles'], st.session_state['graph'], st.session_state['model'] = prerequisites(
-                st.session_state['keywords'])
+    # Step 3: Load Data and Prerequisites
+    if st.session_state['keywords'] and not st.session_state['articles']:
+        st.session_state['articles'], st.session_state['graph'], st.session_state['model'] = prerequisites(st.session_state['keywords'])
 
-            # Call st_selection_pipeline and check the returned value
-            if 'reco_list' not in st.session_state:
-                st.session_state['reco_list'] = st_selection_pipeline(st.session_state.articles,
-                                                                      st.session_state.batch_size,
-                                                                      st.session_state.graph, st.session_state.model)
+    # Step 4: Article Selection and Details Viewing
+    if st.session_state['articles'] and not st.session_state['reco_list']:
+        st.session_state['reco_list'] = st_selection_pipeline(st.session_state['articles'], st.session_state['batch_size'], st.session_state['graph'], st.session_state['model'])
 
-            # Ensure that result is not None
-            if st.session_state.reco_list:
-                st_r_inspection(st.session_state.reco_list, st.session_state.batch_size, st.session_state.graph)
-            else:
-                # Handle the case where result is None (e.g., show a message or take alternative action)
-                st.write("No recommendations available for the given keywords.")
+    # Step 6: View Recommendations
+    if st.session_state['reco_list']:
+        st.write("Do you wish to inspect the most similar articles?")
+        if st.button("Inspect Similar", key="inspect"):
+            st_r_inspection(st.session_state['batch_size'], st.session_state['articles'], st.session_state['reco_list'])
+
 
 if __name__ == "__main__":
     main()
